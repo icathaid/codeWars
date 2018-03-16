b92d6e64f471d559521a688909faac429f0d6aca
James McDaniel <icathaid@gmail.com>
2018-03-15 19:36:19 -0700

    i did everything on master like a BOSS

---
+++ abbreviate/js/app.js
@@ -0,0 +1,6 @@
+var name = "james mcdaniel";
+var name2 = name.split(" ");
+//var initial = `${name2[0].charAt(0).toUpperCase()}.${name2[1].charAt(0).toUpperCase()}.`
+
+var initials = name2[0].charAt(0).toUpperCase() + '.' + name2[1].charAt(0).toUpperCase();
+console.log(initial);
