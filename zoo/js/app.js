c25befdc9b63d992f1108129f4fce6a64748e15a
James McDaniel <icathaid@gmail.com>
2018-03-17 13:45:48 -0700

    image of pseudocode

---
+++ zoo/js/app.js
@@ -0,0 +1,11 @@
+//var names = [];
+//[40, 100, 1, 5, 25, 10]
+
+//var points = ['bill', 'ted', 'alice'];
+//console.log('presorted points: ' + points);
+
+//function myFunction() {
+//    points.sort();
+//    console.log('sorted points: ' + points);
+//};
+//myFunction();
