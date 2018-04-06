5c82dcf5a1ae72646c12175a69910de4bb945bc0
James McDaniel <icathaid@gmail.com>
2018-04-06 00:42:47 -0700

    one bug left, going to bed

--- formatWords/solution.js
+++ formatWords/solution.js
@@ -1,22 +1,37 @@
 'use strict';
 function formatWords(words){
-    let bill = [];
-    if(words.length === 0){
-        return console.log('empty array');
-    } else if(words.length === 1){
-        return words[0];
-    } else if(words.length === 2){
-        return words[0] + ' and ' + words[1];
-        //  need else if for 3 to deal with oxford comma
-    } else if(words.length === 3) {
-        bill.push(words[0] + ', ' + words[1] + ' and ' + words[2]);
-        return bill.join('');
-    } else if(words.length > 3){
-        for(var i = 0; i < (words.length -2); i++){
-            bill.push(words[i] + ', ');
+    let ted = words;
+    for(var i = 0; i < ted.length; i++){
+        if(ted[i].length === 0){
+            ted.splice(i, 1);
         };
-        bill.push(words[words.length -2] + ' and ');
-        bill.push(words[words.length -1]);
+    };
+    let bill = [];
+    if(ted.length === 0){
+        return '';
+    } else if(ted.length === 1){
+        return ted[0];
+    } else if(ted.length === 2){
+        return ted[0] + ' and ' + ted[1];
+    } else if(ted.length === 3) {
+        bill.push(words[0] + ', ' + ted[1] + ' and ' + ted[2]);
+        return bill.join('');
+    } else if(ted.length > 3){
+        for(var i = 0; i < (ted.length -2); i++){
+            bill.push(ted[i] + ', ');
+        };
+        bill.push(ted[ted.length -2] + ' and ');
+        bill.push(ted[ted.length -1]);
         return bill.join('');
     };
+};
+
+function removeCommas(words){
+    let james = words;
+    for(var i = 0; i < james.length; i++){
+        if(james[i].length === 0){
+            james.splice(i, 1);
+        };
+    };
+    return james.join('');
 };
